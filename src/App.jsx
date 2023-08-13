5d6543a62c9b5d8956db9281f32d6a8d6d2fb250
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-08-13 18:22:11 -0300

    feat: about section incomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
     <>
       <Navbar />
       <Home />
+      <About />
     </>
   )
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,35 @@
+import "../styles/stylesComp/about/about.css"
+
+function About() {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="about-content">
+            
+            <div className="text-side">
+              <h3>About me</h3>
+              <h4>
+                A dedicated Fullstack Developer <br /> based in Recife, Brazil
+                📍
+              </h4>
+              <p>
+                As a Fullstack Developer, i possess an impressive arsenal
+                of skills in HTML, CSS, JavaScript, React, Node, and SCSS. I
+                excel in designing and maintaining responsive websites that
+                offer a smooth user experience. My expertise lies in crafting
+                dynamic, engaging interfaces through writing clean and optimized
+                code and utilizing cutting-edge development tools and
+                techniques. I am also a team player who thrives in collaborating
+                with cross-functional teams to produce outstanding web
+                applications.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default About;
